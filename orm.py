6aa5a1e0d0a4f5f36f8b367b5ced0ad6e17574e2
Rand01ph <tanyawei1991@gmail.com>
2016-05-17 09:07:39 +0800

    init

---
+++ orm.py
@@ -0,0 +1,78 @@
+#! /usr/bin/env python
+# coding=utf-8
+# author: Rand01ph
+
+import asyncio, logging
+
+import aiomysql
+
+async def create_pool(loop, **kwargs):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = await aiomysql.create_pool(
+        host=kwargs.get('host', 'localhost'),
+        port=kwargs.get('port', 3306),
+        user=kwargs['user'],
+        password=kwargs['password'],
+        db=kwargs['db'],
+        charset=kwargs.get('charset', 'utf8'),
+        autocommit=kwargs.get('autocommit', True),
+        maxsize=kwargs.get('maxsize', 10),
+        minsize=kwargs.get('minsize', 1),
+        loop=loop
+    )
+
+async def select(sql, args, size=None):
+    logging.info('sql is %s, and args is %s', sql, args)
+    global __pool
+    async with __pool.get() as conn:
+        cur = await conn.cursor(aiomysql.DictCursor)
+        await cur.execute(sql.replace('?', '%s'), args or ())
+        if size:
+            rs = await cur.fetchmany(size)
+        else:
+            rs = await cur.fetchall()
+        await cur.close()
+        logging.info('rows returned: %s' % len(rs))
+        return rs
+
+async def execute(sql, args):
+    logging.info('sql is %s, and args is %s', sql, args)
+    async with __pool.get() as conn:
+        try:
+            cur = await conn.cursor()
+            await cur.execute(sql.replace('?', '%s'), args)
+            affected = cur.rowcount
+            await cur.close()
+        except BaseException as e:
+            raise
+        return affected
+
+
+class Model(dict, metaclass=ModelMetaclass):
+
+
+    def __init__(self, **kwargs):
+        super(Model, self).__init__(**kwargs)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def getValue(self, key):
+        return getattr(self, key, None)
+
+    def getValueOrDefault(self, key):
+        value = getattr(self, key, None)
+        if value is None:
+            field = self.__mappings__[key]
+            if field.default is not None:
+                value = field.default() if callable(field.default) else field.default
+                logging.debug('using default value for %s: %s' % (key, str(value)))
+                setattr(self, key, value)
+        return value
