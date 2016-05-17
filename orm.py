b09ed6c46aa608d6d7fdc35cef8f0f7b5596d057
Rand01ph <361212419@qq.com>
2016-05-17 18:50:14 +0800

    almost finish orm

--- orm.py
+++ orm.py
@@ -49,6 +49,54 @@ async def execute(sql, args):
         return affected
 
 
+class ModelMetaclass(type):
+
+
+    def __new__(cls, name, bases, attrs):
+        # 排除Model本身
+        if name == 'Model':
+            return type.__new__(cls, name, bases, attrs)
+        # 获取表名
+        tableName = attrs.get('__table__', None) or name
+        logging.info('found model:%s (table: %s)', name, tableName)
+        # 获取所有Field和主键名
+        mappings = dict()
+        fields = []
+        primaryKey = None
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                logging.into('found mappings: %s ==> %s', k, v)
+                mappings[k] = v
+                if v.primary_key:
+                    if primaryKey:
+                        raise RuntimeError('Duplicate primary key for field:%s', k)
+                    primaryKey = k
+                else:
+                    fields.append(k)
+            if not primaryKey:
+                raise RuntimeError('Primary not found.')
+            # 排除已添加的Field属性进行下一步处理
+            for k in mappings.keys():
+                attrs.pop(k)
+            # 将定义Fields名转成sql字段名
+            escaped_fields = list(map(lambda f:'`%s`' % f, fields))
+            attrs['__mappings__'] = mappings
+            attrs['__table__'] = tableName
+            attrs['__primary_key__'] = primaryKey
+            attrs['__fields__'] = fields
+            # 构造默认的SELECT INSERT UPDATE DELETE语句
+            attrs['__select__'] = 'select `%s`,%s from `%s`' % (primaryKey, ','.join(escaped_fields), tableName)
+            attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName,
+                                                                               ','.join(escaped_fields),
+                                                                               primaryKey,
+                                                                               create_args_string(len(escaped_fields)+1))
+            attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName,
+                                                                       ','.join(map(lambda f:'`%s`=?' % (mappings.get(f).name or f), fields)),
+                                                                       primaryKey)
+            attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+            return type.__new__(cls, name, bases, attrs)
+
+
 class Model(dict, metaclass=ModelMetaclass):
 
 
@@ -76,3 +124,38 @@ class Model(dict, metaclass=ModelMetaclass):
                 logging.debug('using default value for %s: %s' % (key, str(value)))
                 setattr(self, key, value)
         return value
+
+    @classmethod
+    async def find(cls, pk):
+        'find object by primary key.'
+        rs = await select('%s where `%s`=?' % (cls.__select__, cls__primary_key__), [pk], 1)
+        if len(rs) == 0:
+            return None
+        return cls(**rs[0])
+
+    async def save(self):
+        args = list(map(self.getValueOrDefault, self.__fields__))
+        args.append(self.getValueOrDefault(self.__primary_key__))
+        rows = await execute(self.__insert__, args)
+        if rows != 1:
+            logging.warn('failed to insert record: affected rows: %s' % rows)
+
+
+class Field(object):
+
+
+    def __init__(self, name, column_type, primary_key, default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+
+    def __str__(self):
+        return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
+
+
+class StringField(Field):
+
+
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
