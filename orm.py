f98bc01fff635602705e77ace1b00c764ffaa8fb
Rand01ph <361212419@qq.com>
2016-05-18 13:54:23 +0800

    almost finish orm

--- orm.py
+++ orm.py
@@ -17,9 +17,9 @@ async def create_pool(loop, **kwargs):
         db=kwargs['db'],
         charset=kwargs.get('charset', 'utf8'),
         autocommit=kwargs.get('autocommit', True),
-        maxsize=kwargs.get('maxsize', 10),
+        maxsize=kwargs.get('maxsize', 10), #连接池最大大小
         minsize=kwargs.get('minsize', 1),
-        loop=loop
+        loop=loop #设置消息循环
     )
 
 async def select(sql, args, size=None):
@@ -48,9 +48,56 @@ async def execute(sql, args):
             raise
         return affected
 
+def create_args_string(num):
+    L = []
+    for n in range(num):
+        L.append('?')
+    return ', '.join(L)
+
+
+class Field(object):
+
+    def __init__(self, name, column_type, primary_key, default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+
+    def __str__(self):
+        return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
+
+
+class StringField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
+
+
+class BooleanField(Field):
+
+    def __init__(self, name=None, default=False):
+        super().__init__(name, 'boolean', False, default)
+
+
+class IntegerField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0):
+        super().__init__(name, 'bigint', primary_key, default)
+
+
+class FloatField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, 'real', primary_key, default)
+
+
+class TextField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=None):
+        super().__init__(name, 'text', False, default)
+
 
 class ModelMetaclass(type):
-
 
     def __new__(cls, name, bases, attrs):
         # 排除Model本身
@@ -65,7 +112,7 @@ class ModelMetaclass(type):
         primaryKey = None
         for k, v in attrs.items():
             if isinstance(v, Field):
-                logging.into('found mappings: %s ==> %s', k, v)
+                logging.info('found mappings: %s ==> %s', k, v)
                 mappings[k] = v
                 if v.primary_key:
                     if primaryKey:
@@ -73,32 +120,31 @@ class ModelMetaclass(type):
                     primaryKey = k
                 else:
                     fields.append(k)
-            if not primaryKey:
-                raise RuntimeError('Primary not found.')
-            # 排除已添加的Field属性进行下一步处理
-            for k in mappings.keys():
-                attrs.pop(k)
-            # 将定义Fields名转成sql字段名
-            escaped_fields = list(map(lambda f:'`%s`' % f, fields))
-            attrs['__mappings__'] = mappings
-            attrs['__table__'] = tableName
-            attrs['__primary_key__'] = primaryKey
-            attrs['__fields__'] = fields
-            # 构造默认的SELECT INSERT UPDATE DELETE语句
-            attrs['__select__'] = 'select `%s`,%s from `%s`' % (primaryKey, ','.join(escaped_fields), tableName)
-            attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName,
-                                                                               ','.join(escaped_fields),
-                                                                               primaryKey,
-                                                                               create_args_string(len(escaped_fields)+1))
-            attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName,
-                                                                       ','.join(map(lambda f:'`%s`=?' % (mappings.get(f).name or f), fields)),
-                                                                       primaryKey)
-            attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
-            return type.__new__(cls, name, bases, attrs)
+        if not primaryKey:
+            raise RuntimeError('Primary not found.')
+        # 排除已添加的Field属性进行下一步处理
+        for k in mappings.keys():
+            attrs.pop(k)
+        # 将定义Fields名转成sql字段名
+        escaped_fields = list(map(lambda f:'`%s`' % f, fields))
+        attrs['__mappings__'] = mappings
+        attrs['__table__'] = tableName
+        attrs['__primary_key__'] = primaryKey
+        attrs['__fields__'] = fields
+        # 构造默认的SELECT INSERT UPDATE DELETE语句
+        attrs['__select__'] = 'select `%s`,%s from `%s`' % (primaryKey, ','.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName,
+                                                                            ','.join(escaped_fields),
+                                                                            primaryKey,
+                                                                            create_args_string(len(escaped_fields)+1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName,
+                                                                    ','.join(map(lambda f:'`%s`=?' % (mappings.get(f).name or f), fields)),
+                                                                    primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+        return type.__new__(cls, name, bases, attrs)
 
 
 class Model(dict, metaclass=ModelMetaclass):
-
 
     def __init__(self, **kwargs):
         super(Model, self).__init__(**kwargs)
@@ -128,7 +174,7 @@ class Model(dict, metaclass=ModelMetaclass):
     @classmethod
     async def find(cls, pk):
         'find object by primary key.'
-        rs = await select('%s where `%s`=?' % (cls.__select__, cls__primary_key__), [pk], 1)
+        rs = await select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
         if len(rs) == 0:
             return None
         return cls(**rs[0])
@@ -141,21 +187,3 @@ class Model(dict, metaclass=ModelMetaclass):
             logging.warn('failed to insert record: affected rows: %s' % rows)
 
 
-class Field(object):
-
-
-    def __init__(self, name, column_type, primary_key, default):
-        self.name = name
-        self.column_type = column_type
-        self.primary_key = primary_key
-        self.default = default
-
-    def __str__(self):
-        return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
-
-
-class StringField(Field):
-
-
-    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
-        super().__init__(name, ddl, primary_key, default)
