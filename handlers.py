24d3ac81e5ab8b6aafad63513fa9db808032d427
Rand01ph <361212419@qq.com>
2016-06-12 15:27:12 +0800

    begin to add jinja2

--- handlers.py
+++ handlers.py
@@ -1,22 +1,14 @@
 from aiohttp import web
+import aiohttp_jinja2
 from meepo.core import get, post
+from models import User
 
 
 @get('/')
-def index(a, request):
-    print(request)
-    return web.Response(body=b'<h1>Awesome</h1>')
-
-@post('/')
-def index(a, request):
-    print(request)
-    return web.Response(body=b'<h1>Awesome</h1>')
-
-@get('/blog/{id}')
-def get_blog(id):
-    return web.Response(body=b'<h1>get_blog</h1>')
-
-@get('/ggg')
-def get_ggg(request, *, name):
-    print(request)
-    return web.Response(body=b'<h1>get_blog</h1>')
+@aiohttp_jinja2.template('test.html')
+async def index(request):
+    users = await User.findAll()
+    print(users)
+    return {
+        'users': users
+    }
--- main.py
+++ main.py
@@ -8,10 +8,75 @@ import asyncio, os
 
 from aiohttp import web
 
+import aiohttp_jinja2, jinja2
+
 from meepo.core import add_routes
 
+import meepo.orm as orm
+
+async def logger_factory(app, handler):
+    async def logger(request):
+        logging.info('Request: %s %s' % (request.method, request.path))
+        return await handler(request)
+    return logger
+
+async def data_factory(app, handler):
+    async def parse_data(request):
+        if request.method == 'POST':
+            if request.content_type.startswith('application/json'):
+                request.__data__ = await request.json()
+                logging.info('request json: %s' % str(request.__data__))
+            elif request.content_type.startswith('application/x-www-form-urlencoded'):
+                request.__data__ = await request.post()
+                logging.info('request form: %s' % str(request.__data__))
+        return await handler(request)
+    return parse_data
+
+async def response_factory(app, handler):
+    async def response(request):
+        logging.info('Response handler...')
+        r = await handler(request)
+        if isinstance(r, web.StreamResponse):
+            return r
+        if isinstance(r, bytes):
+            resp = web.Response(body=r)
+            resp.content_type = 'application/octet-stream'
+            return resp
+        if isinstance(r, str):
+            if r.startswith('redirect:'):
+                return web.HTTPFound(r[9:])
+            resp = web.Response(body=r.encode('utf-8'))
+            resp.content_type = 'text/html;charset=utf-8'
+            return resp
+        if isinstance(r, dict):
+            template = r.get('__template__')
+            if template is None:
+                resp = web.Response(body=json.dumps(r, ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'))
+                resp.content_type = 'application/json;charset=utf-8'
+                return resp
+            else:
+                resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
+                resp.content_type = 'text/html;charset=utf-8'
+                return resp
+        if isinstance(r, int) and r >= 100 and r < 600:
+            return web.Response(r)
+        if isinstance(r, tuple) and len(r) == 2:
+            t, m = r
+            if isinstance(t, int) and t >= 100 and t < 600:
+                return web.Response(t, str(m))
+        # default:
+        resp = web.Response(body=str(r).encode('utf-8'))
+        resp.content_type = 'text/plain;charset=utf-8'
+        return resp
+    return response
+
 async def init(loop):
-    app = web.Application(loop=loop)
+    await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='www-data', password='WWW-data-123', db='meepo')
+    app = web.Application(loop=loop, middlewares=[
+        logger_factory, response_factory
+    ])
+    aiohttp_jinja2.setup(app,
+                         loader=jinja2.FileSystemLoader('templates'))
     add_routes(app, 'handlers')
     srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9999)
     logging.info('server started at http://127.0.0.1:999...')
